e3d8ad54416ae3a1a83a9ba640d79890931a7479
Travis Goodwin <travis.goodwin@nih.gov>
2020-03-26 17:51:07 -0400

    Registered eviconv task to the sink

--- fslks/sink.py
+++ fslks/sink.py
@@ -20,11 +20,11 @@ class Input(abc.ABC):
         pass
 
     @abc.abstractmethod
-    def to_tensor(self, elem: typing.Mapping[str, tf.Tensor]) -> tf.Tensor:
+    def to_tensor(self, elem: tfds.features.FeaturesDict) -> tf.Tensor:
         pass
 
     @abc.abstractmethod
-    def to_str(self, elem: typing.Mapping[str, tf.Tensor]) -> str:
+    def to_str(self, elem: tfds.features.FeaturesDict) -> str:
         pass
 
 
@@ -32,10 +32,10 @@ class Constant(Input):
     def __init__(self, value: str):
         self._value = value
 
-    def to_tensor(self, elem: typing.Mapping[str, tf.Tensor]) -> tf.Tensor:
+    def to_tensor(self, elem: tfds.features.FeaturesDict) -> tf.Tensor:
         return tf.constant(self._value, dtype=tf.string, name='sink.Constant')
 
-    def to_str(self, elem: typing.Mapping[str, tf.Tensor]) -> str:
+    def to_str(self, elem: tfds.features.FeaturesDict) -> str:
         return str(self._value)
 
 
@@ -46,11 +46,26 @@ class Feature(Input):
     def validate(self, info: tfds.core.DatasetInfo) -> None:
         assert self._key in info.features
 
-    def to_tensor(self, elem: typing.Mapping[str, tf.Tensor]) -> tf.Tensor:
+    def to_tensor(self, elem: tfds.features.FeaturesDict) -> tf.Tensor:
         return elem[self._key]
 
-    def to_str(self, elem: typing.Mapping[str, tf.Tensor]) -> str:
+    def to_str(self, elem: tfds.features.FeaturesDict) -> str:
         return str(elem[self._key].numpy())
+
+
+class DictEntry(Input):
+    def __init__(self, dict_feature: str, entry_mapper: Input):
+        self.dict_feature = dict_feature
+        self.entry_mapper = entry_mapper
+
+    def validate(self, info: tfds.core.DatasetInfo) -> None:
+        assert self.dict_feature in info.features
+
+    def to_tensor(self, elem: tfds.features.FeaturesDict) -> tf.Tensor:
+        return self.entry_mapper.to_tensor(elem[self.dict_feature])
+
+    def to_str(self, elem: tfds.features.FeaturesDict) -> str:
+        return self.entry_mapper.to_str(elem[self.dict_feature])
 
 
 class LabelMapping(Input):
@@ -62,10 +77,10 @@ class LabelMapping(Input):
         assert self.label in info.features
         [input_.validate(info) for input_ in self.mapping.values()]
 
-    def to_tensor(self, elem: typing.Mapping[str, tf.Tensor]) -> tf.Tensor:
+    def to_tensor(self, elem: tfds.features.FeaturesDict) -> tf.Tensor:
         return self.mapping[int(elem[self.label].numpy())].to_tensor(elem)
 
-    def to_str(self, elem: typing.Mapping[str, tf.Tensor]) -> str:
+    def to_str(self, elem: tfds.features.FeaturesDict) -> str:
         return self.mapping[int(elem[self.label].numpy())].to_str(elem)
 
 
@@ -73,7 +88,7 @@ class Sequence(Input):
     def __init__(self, inputs: typing.Union[str, typing.Iterable[Input]]):
         self._inputs = inputs
 
-    def validate(self, info: tfds.core.DatasetInfo) -> None:
+    def validate(self, info: tfds.features.FeaturesDict) -> None:
         if isinstance(self._inputs, str):
             assert self._inputs in info.features
         elif isinstance(self._inputs, typing.Iterable):
@@ -82,7 +97,7 @@ class Sequence(Input):
             # This shouldn't happen if Python is correctly type checking!
             raise ValueError
 
-    def to_tensor(self, elem: typing.Mapping[str, tf.Tensor]) -> tf.Tensor:
+    def to_tensor(self, elem: tfds.features.FeaturesDict) -> tf.Tensor:
         if isinstance(self._inputs, str):
             inputs = Feature(self._inputs)
         elif isinstance(self._inputs, typing.Iterable):
@@ -117,8 +132,8 @@ def register(dataset_name: str, prompt: Input, input: Input, output: Input):
 
     def make_conversion_fn(encoder_fn):
 
-        def conversion_fn(elem: typing.Mapping[str, tf.Tensor]):
-            ex = encoder_fn(prompt.to_str(elem) + ' ' + input.to_str(elem))
+        def conversion_fn(elem: tfds.features.FeaturesDict):
+            ex = encoder_fn(prompt.to_str(elem) + ' : ' + input.to_str(elem))
             outputs = encoder_fn(output.to_str(elem))['input_ids']
             return ex, outputs
 
--- fslks/tasks.py
+++ fslks/tasks.py
@@ -1,7 +1,7 @@
 from fslks import sink
 
 sink.register('bioasq',
-              prompt=sink.Constant('summarize:'),
+              prompt=sink.Constant('summarize'),
               input=sink.Sequence([
                   sink.Feature('question'),
                   sink.Feature('article')
@@ -9,24 +9,26 @@ sink.register('bioasq',
               output=sink.Feature('summary'))
 
 sink.register('scientific_papers',
-              prompt=sink.Constant('summarize:'),
+              prompt=sink.Constant('summarize'),
               input=sink.Feature('article'),
               output=sink.Feature('abstract'))
 
 sink.register('movie_rationales',
-              prompt=sink.Constant('summarize:'),
+              prompt=sink.Constant('summarize'),
               input=sink.Sequence('evidences'),
               output=sink.Feature('review'))
 
 sink.register('cnn_dailymail',
-              prompt=sink.Constant('summarize:'),
+              prompt=sink.Constant('summarize'),
               input=sink.Feature('article'),
               output=sink.Feature('highlights'))
 
 sink.register('super_glue/copa',
-              prompt=sink.Constant('choose:'),
-              input=sink.Sequence([
+              prompt=sink.Sequence([
+                  sink.Constant('choose'),
                   sink.Feature('question'),
+              ]),
+              input=sink.Sequence([
                   sink.Feature('premise'),
                   sink.Feature('choice1'),
                   sink.Feature('choice2'),
@@ -34,4 +36,21 @@ sink.register('super_glue/copa',
               output=sink.LabelMapping('label', {
                   0: sink.Feature('choice1'),
                   1: sink.Feature('choice2')
+              }))
+
+_eviconv_stance_mapping = sink.LabelMapping('stance', {
+    0: sink.Constant('PRO :'),
+    1: sink.Constant('CON :'),
+})
+sink.register('eviconv',
+              prompt='argue',
+              input=sink.Sequence([
+                  sink.DictEntry('evidence_1', _eviconv_stance_mapping),
+                  sink.DictEntry('evidence_1', sink.Feature('text')),
+                  sink.DictEntry('evidence_2', _eviconv_stance_mapping),
+                  sink.DictEntry('evidence_2', sink.Feature('text')),
+              ]),
+              output=sink.LabelMapping('_LABEL', {
+                  0: sink.DictEntry('evidence_1', sink.Feature('text')),
+                  1: sink.DictEntry('evidence_2', sink.Feature('text'))
               }))
